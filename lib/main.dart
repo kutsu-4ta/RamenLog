e6211149307558880854294b56825d56549f687b
yamashita <yamashita.antapp@gmail.com>
2023-06-27 23:56:43 +0900

    feature: home画面_新UIに変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'View/HomePage.dart';
 import 'Consts/app.dart';
 import 'package:camera/camera.dart';
+import 'provider/providers.dart';
 
 void main() async {
   // main 関数内で非同期処理を呼び出すための設定
   WidgetsFlutterBinding.ensureInitialized();
   // デバイスで使用可能なカメラのリストを取得
-  final cameras = await availableCameras();
+  // final cameras = await availableCameras();
   // 利用可能なカメラのリストから特定のカメラを取得
   // final firstCamera = cameras.first;
-  runApp(MyApp(
+  runApp(ProviderScope(child: MyApp(
       // camera: firstCamera
-  ));
+  )));
 }
 
 class MyApp extends StatelessWidget {
